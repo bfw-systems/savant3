766882133f00ae32d1256d3756a834bacaec3087
bulton-fr <bulton.fr@gmail.com>
2014-11-11 01:38:28 +0100

    Initial commit

---
+++ src/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Fichier de configuration du module bfw-sql
+ * @author Vermeulen Maxime <bulton.fr@gmail.com>
+ * @package bfw-sql
+ * @version 1.0
+ */
+
+//*** Savant3 configs ***
+
+/*
+ * Dossier où sont les fichiers template pour savant3 par rapport à la racine du projet
+ * Exemple : 'view/tpl'
+ */
+$template_dir = '';
+
+//*** Savant3 configs *** 
