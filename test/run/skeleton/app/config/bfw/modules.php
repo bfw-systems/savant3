f33c6aecb9c3b6653f6ec45202eec924acadd22f
bulton-fr <bulton.fr@gmail.com>
2018-07-25 18:41:22 +0200

    Test run - bfw config file : Compatibility with rc6

--- test/run/skeleton/app/config/bfw/modules.php
+++ test/run/skeleton/app/config/bfw/modules.php
@@ -1,6 +1,6 @@
 <?php
 
-$config = require(__DIR__.'/config.php.original');
+$config = require(__DIR__.'/modules.php.original');
 
 $config['modules']['template']['name']    = 'bfw-savant3';
 $config['modules']['template']['enabled'] = true;
